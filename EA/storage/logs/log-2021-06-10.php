05ccee8d3f6fdb3350051c84b752d1a75c2cf446
vzaviation <pswanso2@gmail.com>
2021-06-10 20:53:25 -0500

    updated code
    06/10/2021 changes

---
+++ EA/storage/logs/log-2021-06-10.php
@@ -0,0 +1,10 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2021-06-10 00:54:03 --> Severity: Warning --> mysqli::real_connect(): (HY000/2002): No such file or directory /Users/peterswanson/Sites/localhost/EA/vendor/codeigniter/framework/system/database/drivers/mysqli/mysqli_driver.php 203
+ERROR - 2021-06-10 00:54:03 --> Unable to connect to the database
+ERROR - 2021-06-10 01:25:28 --> Severity: Warning --> mysqli::real_connect(): (HY000/2002): No such file or directory /Users/peterswanson/Sites/localhost/EA/vendor/codeigniter/framework/system/database/drivers/mysqli/mysqli_driver.php 203
+ERROR - 2021-06-10 01:25:28 --> Unable to connect to the database
+ERROR - 2021-06-10 01:57:40 --> Severity: Warning --> mysqli::real_connect(): (HY000/2002): No such file or directory /Users/peterswanson/Sites/localhost/EA/vendor/codeigniter/framework/system/database/drivers/mysqli/mysqli_driver.php 203
+ERROR - 2021-06-10 01:57:40 --> Unable to connect to the database
+ERROR - 2021-06-10 03:52:42 --> Severity: Warning --> mysqli::real_connect(): (HY000/2002): No such file or directory /Users/peterswanson/Sites/localhost/EA/vendor/codeigniter/framework/system/database/drivers/mysqli/mysqli_driver.php 203
+ERROR - 2021-06-10 03:52:42 --> Unable to connect to the database
